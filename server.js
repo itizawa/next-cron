fbd7ea1809eb441c8038d8272a3675380d94f544
itizawa <itizawa@weseek.co.jp>
2020-08-14 18:23:58 +0900

    create NextCronApp class

--- server.js
+++ server.js
@@ -2,18 +2,65 @@ const express = require('express');
 const next = require('next');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express();
+class NextCronApp {
 
-  server.all('*', (req, res) => {
-    return handle(req, res);
-  });
+  constructor() {
+    this.nextApp = undefined;
+    this.expressApp = undefined;
+  }
 
-  server.listen(port, (err) => {
+  getExpressApp() {
+    return this.expressApp;
+  }
+
+  /**
+   * initialize
+   */
+  async init() {
+    this.nextApp = this.initNextJs();
+
+    await this.nextApp.prepare();
+
+
+    this.expressApp = express();
+    await this.initRoutes(this.nextApp, this.expressApp);
+  }
+
+  /**
+   * Initialize Next.js
+   */
+  initNextJs() {
+    return next({
+      dir: '.',
+      dev: (process.env.NODE_ENV !== 'production'),
+    });
+  }
+
+  /**
+   * initialize Express instance
+   * @param {object} nextApp
+   * @param {object} expressApp
+   */
+  initRoutes(nextApp, expressApp) {
+
+    // Default catch-all handler to allow Next.js to handle all other routes
+    expressApp.all('*', async(req, res) => {
+      const nextRequestHandler = nextApp.getRequestHandler();
+      return nextRequestHandler(req, res);
+    });
+  }
+
+}
+
+/** **********************************
+ *          Main Process
+ ********************************** */
+
+const nextCronApp = new NextCronApp();
+nextCronApp.init().then(() => {
+  const expressApp = nextCronApp.getExpressApp();
+  expressApp.listen(port, (err) => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
