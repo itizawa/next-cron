c399f9f08834d04ed0f1c590c9812bfb791f0f35
itizawa <itizawa@weseek.co.jp>
2020-08-15 18:31:15 +0900

    optimisation

--- useCase.js
+++ useCase.js
@@ -3,15 +3,12 @@ const youtubeCronService = require('./services/youtube-cron-service');
 
 async function main() {
   await youtubeCronService.init();
-  // const channelIds = await youtubeCronService.getChannelIds();
-
-  // TODO テスト ids
-  const channelIds = ['UC1EB8moGYdkoZQfWHjh7Ivw', 'UCFo4kqllbcQ4nV83WCyraiw'];
-
-  const newPlaylist = await youtubeCronService.createNewPlaylist();
+  const [channelIds, newPlaylist] = await Promise.all([
+    youtubeCronService.getChannelIds(),
+    youtubeCronService.createNewPlaylist(),
+  ]);
 
   const videoIds = await youtubeCronService.retrieveNewVideoIdsBySubscriptionId(channelIds);
-
   await youtubeCronService.insertVideosToPlayList(videoIds, newPlaylist.id);
 
 }
