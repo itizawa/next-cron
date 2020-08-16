bfd06037c86cdbb24b567ddc5927f349876e90e1
itizawa <itizawa@weseek.co.jp>
2020-08-16 12:54:54 +0900

    test

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,6 +7,7 @@ export default function Home() {
       <h1>Hello</h1>
       <p>show my page</p>
       <a href="https://itizawa-tech.growi.cloud/5f3644fc2d4e0b00485c1d96">https://itizawa-tech.growi.cloud/next-cron</a>
+      {process.env.ENABLE_TIME_SIGNAL}
     </>
   );
 }
