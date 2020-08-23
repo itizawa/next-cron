d136af6ae36365cc61cc460fe35d7c9d6c191a8f
itizawa <itizawa@weseek.co.jp>
2020-08-23 13:28:12 +0900

    adjust

--- services/qiita-trend.js
+++ services/qiita-trend.js
@@ -32,21 +32,33 @@ class QiitaTrendService {
   }
 
   setupSchedule() {
-    this.qiitaTrendRetrieveJob = schedule.scheduleJob('0 0 */3 * * *', async() => {
+    this.qiitaTrendRetrieveJob = schedule.scheduleJob('0 0 14 * * *', async() => {
       // eslint-disable-next-line no-console
       console.log(`QiitaTrendService: fire github grass service ${new Date()}`);
 
       const res = await axios.get(`https://qiita.com/api/v2/items?page=1&per_page=100&query=${this.searchText}`);
-      this.dayliyArticle = res.data.map((item) => {
-        return `${item.title} \n ${item.url}`;
+      const articles = res.data.map((item) => {
+        return {
+          title: item.title,
+          url: item.url,
+          likes: item.likes_count,
+        };
       });
+
+      articles.sort((a, b) => {
+        if (a.likes > b.likes) return -1;
+        if (a.likes < b.likes) return 1;
+        return 0;
+      });
+
+      this.dayliyArticle = articles;
 
       // reset index
       this.index = 0;
 
     });
 
-    this.qiitaNotifySlackJob = schedule.scheduleJob('0 */15 * * * *', async() => {
+    this.qiitaNotifySlackJob = schedule.scheduleJob('0 */20 * * * *', async() => {
       if (this.dayliyArticle == null) {
         // eslint-disable-next-line no-console
         return console.log('QiitaTrendService: dayliy article is null');
@@ -54,8 +66,10 @@ class QiitaTrendService {
       // eslint-disable-next-line no-console
       console.log(`QiitaTrendService: fire qiita notify to slack ${new Date()}`);
 
+      const article = this.dayliyArticle[this.index];
+
       this.nc.slackNotificationService.fire(
-        this.slackChannel, 'Qiita Trend', `${this.dayliyArticle[this.index]}`, ':memo:',
+        this.slackChannel, 'Qiita Trend', `${article.title} \n ${article.url}`, ':memo:',
       );
 
       this.index += 1;
